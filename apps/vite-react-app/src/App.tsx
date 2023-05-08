a858f3c11f649bd38f31cb5cacdfa3c3eeb13cf8
BTech LLC <mikhail@bashchkov.com>
2023-05-08 17:26:40 +0400

    Update

--- apps/vite-react-app/src/App.tsx
+++ apps/vite-react-app/src/App.tsx
@@ -1,9 +1,11 @@
 import { Button } from '@bashchkov/mdui'
+import {MyButton} from '@bashchkov/my-lib'
 
 function App() {
   return (
     <>
       <Button />
+      <MyButton>Hi sdfsdfsdfd</MyButton>
     </>
   )
 }
